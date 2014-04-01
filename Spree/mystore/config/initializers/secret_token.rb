63a21c5c801cb489ab5432f0a5a4b7ccb1145d7e
ereyesg <ereyes1@uc.cl>
2014-04-01 19:06:44 -0300

    Proyecto prueba ecommerce

---
+++ Spree/mystore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '720a42b9dede2c76113ded86884bd917f9105af38913f34cccaa784718e1b8c4739983f2a7ef4abf453bf1e37d0964bb2a0d9441d2ce5f481bf76f6c505df2c0'
