a3d5c3892c9306d55f9c26856aa353d2f1c99c2a
ereyesg <ereyes1@uc.cl>
2014-04-11 17:53:44 -0300

    hola

--- Spree/mystore/db/schema.rb
+++ Spree/mystore/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401235523) do
+ActiveRecord::Schema.define(version: 20140411180511) do
 
   create_table "spree_addresses", force: true do |t|
     t.string   "firstname"
