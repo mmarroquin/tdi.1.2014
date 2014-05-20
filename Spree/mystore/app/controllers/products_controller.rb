cc0e730f59274518d879c2b03bf80247e876fef1
valentina <valentina@valentina-VirtualBox.(none)>
2014-05-20 14:49:05 -0400

    read json

--- Spree/mystore/app/controllers/products_controller.rb
+++ Spree/mystore/app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @var = WebProduct.read
   end
 
   # GET /products/1
--- Spree/mystore/app/models/web_product.rb
+++ Spree/mystore/app/models/web_product.rb
@@ -1,2 +1,14 @@
+require 'json'
 class WebProduct < ActiveRecord::Base
+	def self.read
+		file = open ("/home/valentina/productos(1).json")
+		json = file.read
+		product = ActiveSupport::JSON.decode(json)
+		a = 0
+		product.each do |aux|
+  			#Product.create(:sku=>aux['sku'], :description=>aux['sku'], :start_date=>columns[3], :final_date=>columns[4])
+  			a = aux['sku']
+		end
+		return a
+	end
 end
