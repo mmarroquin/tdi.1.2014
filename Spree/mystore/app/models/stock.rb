55e36bf177062fdd6d2945e79f993548d8b4f0ee
Esteban Schirmer <eischirm@gmail.com>
2014-06-10 15:00:54 -0400

    ultimas correcciones

--- Spree/mystore/app/models/stock.rb
+++ Spree/mystore/app/models/stock.rb
@@ -203,7 +203,7 @@ class Stock < ActiveRecord::Base
 
 	    	price_prod = Product.last(:select => "products.price", :conditions => ['start_date <= ? AND final_date >= ? AND sku = ?', Date.current, Date.current, prod[:sku] ])
 		    if price_prod == nil
-		    	price_prod = WebProduct.last(:select => "webproducts.price_normal", :conditions => ['sku = ?', prod[:sku] ])
+		    	price_prod = WebProduct.last(:select => "web_products.price_normal", :conditions => ['sku = ?', prod[:sku] ])
 		    end
 		    cantidadProd = prod[:cant].to_i
 		    if responseDespacho.find { |producto| producto['_id'] == prod[:sku] }
