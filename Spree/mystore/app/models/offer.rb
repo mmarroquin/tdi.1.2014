e787971f2dc1a5365ecb881a303139405a7a4f55
valentina <valentina@valentina-VirtualBox.(none)>
2014-06-21 23:39:21 -0400

    nuevos cambios oferta

--- Spree/mystore/app/models/offer.rb
+++ Spree/mystore/app/models/offer.rb
@@ -20,31 +20,9 @@ class Offer < ActiveRecord::Base
 	end
 
 	def self.segundo_antes_primero(ahora, inicio)
-		if ahora.year > inicio.year
-			return true
-		else
-			if ahora.month > inicio.month
-				return true
-			else
-				if ahora.day > inicio.day
-					return true
-				else
-					if ahora.hour > inicio.hour
-						return true
-					else
-						if ahora.min > inicio.min
-							return true
-						else
-							if ahora.sec > inicio.sec
-								return true
-							end
-						end
-					end
-				end
-			end
-		end
-
-		return false
+		a =ahora.to_i
+		i= inicio.to_i
+		return (a>i)
 	end
 
 	def self.cambiarPrecio
--- Spree/mystore/app/models/reposicion.rb
+++ Spree/mystore/app/models/reposicion.rb
@@ -14,30 +14,9 @@ class Reposicion < ActiveRecord::Base
 	end
 
 	def self.segundo_antes_primero(ahora, inicio)
-		if ahora.year > inicio.year
-			return true
-		else
-			if ahora.month > inicio.month
-				return true
-			else
-				if ahora.day > inicio.day
-					return true
-				else
-					if ahora.hour > inicio.hour
-						return true
-					else
-						if ahora.min > inicio.min
-							return true
-						else
-							if ahora.sec > inicio.sec
-								return true
-							end
-						end
-					end
-				end
-			end
-		end
-		return false
+		a =ahora.to_i
+		i= inicio.to_i
+		return (a>i)
 	end
 
 end
