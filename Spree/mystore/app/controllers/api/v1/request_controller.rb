31e44e4d14405a70c74e7b102af34055ff3fc4ba
Esteban Schirmer <eischirm@gmail.com>
2014-06-22 00:59:54 -0400

    arreglo api

--- Spree/mystore/app/controllers/api/v1/request_controller.rb
+++ Spree/mystore/app/controllers/api/v1/request_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::RequestController < ApplicationController
   $permisos = {"grupo9" => "QWhiPGn2Hnm54", "grupo6" => "ebdf1bdb858ced98b4adef024c3ec86fbdc141c9", "Grupo2" => "qwertyiop"}
   # POST /api/v1/pedirProducto
   def create
-    if params[:password] == permisos[params[:usuario]]
+    if params[:password] == $permisos[params[:usuario]]
     @resultado = Request.pedirProducto(params[:almacenId], params[:sku], params[:cant])
     render json: @resultado
     else
