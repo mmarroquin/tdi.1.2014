8f8051ab1725e8424c71b53172820a9444cb9e48
matias <matias@matias-VirtualBox.(none)>
2014-06-21 23:37:43 -0400

    asdf

--- Spree/mystore/db/schema.rb
+++ Spree/mystore/db/schema.rb
@@ -825,6 +825,11 @@ ActiveRecord::Schema.define(version: 20140613183027) do
     t.datetime "updated_at"
   end
 
+  create_table "tweets", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "web_products", force: true do |t|
     t.string   "sku"
     t.string   "description"
