3d69aa676adc19416dd4322225faafac1741c0ef
valentina <valentina@valentina-VirtualBox.(none)>
2014-06-22 22:09:41 -0400

    offer

--- Spree/mystore/app/models/rabbitmpq.rb
+++ Spree/mystore/app/models/rabbitmpq.rb
@@ -17,7 +17,7 @@ class Rabbitmpq < ActiveRecord::Base
 
 		ch = conn.create_channel
 		q = ch.queue("reposicion",:auto_delete=>true)
-		while q.message_count>2000
+		while q.message_count>0
 			content1 = q.pop do |delivery_info, properties, body|
 				repos = JSON.parse(body)
 				Reposicion.create(:sku=>repos['sku'], :almacenId=>repos['almacenId'], :fecha=>DateTime.strptime((repos["fecha"]/1000).to_s,"%s"), :fueRepuesto =>false)
@@ -32,7 +32,7 @@ class Rabbitmpq < ActiveRecord::Base
 
 		ch = conn.create_channel
 		q = ch.queue("ofertas",:auto_delete=>true)
-		while q.message_count>90
+		while q.message_count>0
 			content1 = q.pop do |delivery_info, properties, body|
 				oferta = JSON.parse(body)
 				Offer.create(:sku=>oferta['sku'], :precio=>oferta['precio'], :inicio=>DateTime.strptime((oferta["inicio"]/1000).to_s,"%s"), :fin=>DateTime.strptime((oferta["fin"]/1000).to_s,"%s"), :fuePublicado=>false, :TienePrecioBase=>true)
