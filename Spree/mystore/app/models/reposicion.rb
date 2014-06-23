b9c97618d39052eaa7bd8ebf85b7f927fc167b3a
valentina <valentina@valentina-VirtualBox.(none)>
2014-06-22 22:24:49 -0400

    rep

--- Spree/mystore/app/models/reposicion.rb
+++ Spree/mystore/app/models/reposicion.rb
@@ -6,10 +6,10 @@ class Reposicion < ActiveRecord::Base
 		rep.each do |repos|
 			if segundo_antes_primero(fActual,repos.fecha)
 				almacen =repos.almacenId
-				sku = repos.sku
+				sku = repos.sku.to_s
 				id=repos.id
 				Reposicion.where(:id=>id).last.update_attributes(:fueRepuesto=>true)
-				Stock.movStockUn (almacen, sku)
+				Stock.movStockUn(almacen,sku)
 			end 
 		end
 	end
