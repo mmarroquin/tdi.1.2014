94d62d3160560d7c6df63a4c36f86beb11fdb748
administrator <administrator@integra1.ing.puc.cl>
2014-06-28 11:12:08 -0400

    hola

--- Spree/mystore/db/schema.rb
+++ Spree/mystore/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623182417) do
+ActiveRecord::Schema.define(version: 20140628135635) do
 
   create_table "dashboards", force: true do |t|
     t.datetime "created_at"
@@ -57,6 +57,11 @@ ActiveRecord::Schema.define(version: 20140623182417) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "broked"
+  end
+
+  create_table "pasar2s", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "products", force: true do |t|
